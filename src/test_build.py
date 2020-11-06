31c9df02cf92cbcf4f357db3d7da9ab516654085
Georg Hess <hessgeorg9@gmail.com>
2020-11-06 09:43:18 +0100

    updates

--- src/test_build.py
+++ src/test_build.py
@@ -5,18 +5,18 @@ import numpy as np
 import math
 
 MAX_NUMBER_OF_OBSTACLES = 10
+MAX_SOVLER_TIME = 10_000_000
 
-(nu, nz, N) = (2, 6, 30) # v and w decision, 3 init and 3 end positions as parameter, 20 time steps
+(nu, nz, N) = (2, 6, 70) # v and w decision, 3 init and 3 end positions as parameter, 30 time steps
 nedges = 4
 (nobs, Nobs) = (2+2*nedges, MAX_NUMBER_OF_OBSTACLES) #x,y, 4 phis and 4 psis
-ts = 0.1
-(q, qtheta, r, qN, qthetaN) = (10, 0.1, 1, 200, 2)
+ts = 0.2
+(q, qtheta, rv, rw, qN, qthetaN) = (0.1, 0, 0.1, 0.1, 2000, 0)
 
 def build():
     # Build parametric optimizer
     # ------------------------------------
     
-
     u = cs.SX.sym('u', nu*N)
     z0 = cs.SX.sym('z0', nz+Nobs*nobs+1)
 
@@ -29,28 +29,28 @@ def build():
     for t in range(0, nu*N, nu): # LOOP OVER TIME STEPS
         cost += q*((x-xref)**2 + (y-yref)**2) + qtheta*(theta-thetaref)**2
         u_t = u[t:t+nu]
-        cost += r * cs.dot(u_t, u_t)
+        cost += rv * u_t[0]**2 + rw * u_t[1] ** 2
         x += ts * (u_t[0] * cs.cos(theta))
         y += ts * (u_t[0] * cs.sin(theta))
         theta += ts * u_t[1]
 
 
         for i in range(0, Nobs*nobs, nobs): # LOOP OVER ALL OBSTACLES
-            xi, yi = (z0[nz+i], z0[nz+i+1])
+            xi, yi = (z0[nz+i], z0[nz+i+1]) # Center of obstacle i
             xdiff = x-xi
             ydiff = y-yi
-            angle = cs.fmod(cs.atan2(ydiff, xdiff),2.0*cs.pi)
+            angle = cs.fmod(cs.atan2(ydiff, xdiff),2.0*cs.pi) # angle between obstacle and current position
             
             phi_sum_nearest = cs.SX.zeros(1,1)
             smallest_angle_diff = cs.pi*2.0
             
             psi_nearest = cs.SX.zeros(1,1)
 
-            for j in range(0,nedges): # LOOP OVER ALL REGIONS IN OBSTACLE i
+            for j in range(0,nedges): # LOOP OVER ALL EDGES IN OBSTACLE i
 
-                psi = z0[nz+i+2+nedges+j]
+                psi = z0[nz+i+2+nedges+j] # distance for current edge
                 
-                phi_sum = cs.dot(z0[nz+i+2:nz+i+2+j+1],cs.SX.ones(j+1,1))
+                phi_sum = cs.dot(z0[nz+i+2:nz+i+2+j+1],cs.SX.ones(j+1,1)) # acccum angle for current edge
                 temp_diff = cs.fmod(phi_sum - angle, 2.0*cs.pi)
                 temp_bool = temp_diff < smallest_angle_diff
                 psi_nearest = cs.if_else(temp_bool, psi, psi_nearest)
@@ -68,8 +68,8 @@ def build():
 
     cost += qN*((x-xref)**2 + (y-yref)**2) + qthetaN*(theta-thetaref)**2
 
-    umin = [-1.4] * (nu*N) 
-    umax = [1.4] * (nu*N) 
+    umin = [-1.5] * (nu*N) 
+    umax = [1.5] * (nu*N) 
     bounds = og.constraints.Rectangle(umin, umax)
 
     problem = og.builder.Problem(u, z0, cost).with_aug_lagrangian_constraints(c, og.constraints.Zero(), og.constraints.Zero()) \
@@ -81,34 +81,82 @@ def build():
     meta = og.config.OptimizerMeta()\
         .with_optimizer_name("navigation")
     solver_config = og.config.SolverConfiguration()\
-        .with_tolerance(1e-5)
+            .with_tolerance(1e-5)\
+            .with_max_duration_micros(MAX_SOVLER_TIME)
     builder = og.builder.OpEnOptimizerBuilder(problem, 
                                             meta,
                                             build_config, 
                                             solver_config) \
         .with_verbosity_level(1)
     builder.build()
+def x_y_from_rect(rect): 
+    return (rect[0]-rect[-1], rect[0]+rect[-1], rect[1]-rect[-1], rect[1]+rect[-1])
+
+def plot_trajectory(u, x_init):
+
+    rect1 = [1, 1, 0, cs.pi/2, cs.pi/2, cs.pi/2, 0.5, 0.5, 0.5, 0.5]
+    xmin, xmax, ymin, ymax = x_y_from_rect(rect1)
+    rectangles = list(np.zeros(Nobs*nobs))
+    rectangles[0:nobs] = rect1
+    rect2 = [0, -1.5, 0, cs.pi/2, cs.pi/2, cs.pi/2, 0.5, 0.5, 0.5, 0.5]
+    xmin1, xmax1, ymin1, ymax1 = x_y_from_rect(rect2)
+    rectangles[nobs:2*nobs] = rect2
+
+    N = len(u)//nu
+    nx = nz//2
+    x_states = [0.0] * (nx*(N+2))
+    x_states[0:nx+1] = x_init
+    for t in range(0, N):
+        u_t = u[t*nu:(t+1)*nu]
+
+        x = x_states[t * nx]
+        y = x_states[t * nx + 1]
+        theta = x_states[t * nx + 2]
+
+        theta_dot = u_t[1]
+
+        x_states[(t+1)*nx] = x + ts * (u_t[0] * cs.cos(theta))
+        x_states[(t+1)*nx+1] = y + ts * (u_t[0] * cs.sin(theta))
+        x_states[(t+1)*nx+2] = theta + ts*theta_dot
+
+    xx = x_states[0:nx*N:nx]
+    xy = x_states[1:nx*N:nx]
+
+    plt.subplot(313)
+    plt.plot(xx, xy, '-o')
+    plt.plot([xmin,xmax,xmax,xmin, xmin], [ymin, ymin, ymax, ymax, ymin])
+    plt.plot([xmin1,xmax1,xmax1,xmin1, xmin1], [ymin1, ymin1, ymax1, ymax1, ymin1])
+    plt.axis('equal')
+
+    plt.show()
 
 def run():
     # Use TCP server
     # ------------------------------------
+    x_init = [-2.0, -2.0, math.pi/4]
+    x_finish = [2, 2, math.pi/4]
+    initial_guess = [1.5, 0.0] * 7 + [1.5, -cs.pi/4/0.2/3] * 3 + [1.5, 0.0] * 4 + [0.5, cs.pi/4/0.2/3] * 6 + [1.5, 0] * 7
+    initial_guess += (N*nu-len(initial_guess))*[0.0]
+    #plot_trajectory(inital_guess, x_init)
+
     mng = og.tcp.OptimizerTcpManager('python_test_build/navigation')
     mng.start()
 
     mng.ping()
 
-    x_init = [-2.0, -2.0, math.pi/4]
-    x_finish = [2, 2, math.pi/4]
+    
 
-    xmin, xmax, ymin, ymax = (0.5, 1.5, 0.5, 1.5)
+    rect1 = [1, 1, 0, cs.pi/2, cs.pi/2, cs.pi/2, 0.5, 0.5, 0.5, 0.5]
+    xmin, xmax, ymin, ymax = x_y_from_rect(rect1)
     rectangles = list(np.zeros(Nobs*nobs))
-    rectangles[0:nobs] = [0, 0, 0, 1*2*cs.pi/4, 2*2*cs.pi/4, 3*2*cs.pi/4, 0.5, 0.5, 0.5, 0.5]
-    xmin1, xmax1, ymin1, ymax1 = (-0.5, 0.5, -2, -1)
-    #rectangles[nobs:2*nobs] = [0, -1.5, 0, 1*2*cs.pi/4, 2*2*cs.pi/4, 3*2*cs.pi/4, 0.5, 0.5, 0.5, 0.5]
+    rectangles[0:nobs] = rect1
+    rect2 = [0, -1.5, 0, cs.pi/2, cs.pi/2, cs.pi/2, 0.5, 0.5, 0.5, 0.5]
+    xmin1, xmax1, ymin1, ymax1 = x_y_from_rect(rect2)
+    rectangles[nobs:2*nobs] = rect2
 
-    parameters = x_init+x_finish+rectangles+[1]
-
-    solution = mng.call(parameters, initial_guess=[1.0, 0.0] * (nu*N//2))
+    parameters = x_init+x_finish+rectangles+[2]
+    
+    solution = mng.call(parameters, initial_guess=initial_guess)
     mng.kill()
 
     print(f'Solution time: {solution["solve_time_ms"]}')
@@ -133,7 +181,8 @@ def run():
 
     # Plot trajectory
     # ------------------------------------
-    nx = nz//2
+    plot_trajectory(u_star, x_init)
+    '''nx = nz//2
     x_states = [0.0] * (nx*(N+2))
     x_states[0:nx+1] = x_init
     for t in range(0, N):
@@ -158,10 +207,10 @@ def run():
     plt.plot([xmin1,xmax1,xmax1,xmin1, xmin1], [ymin1, ymin1, ymax1, ymax1, ymin1])
     plt.axis('equal')
 
-    plt.show()
+    plt.show()'''
 
 if __name__ == '__main__':
-    do_build = False
+    do_build = True
     do_run = True
 
     if do_build:
