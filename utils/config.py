c45ba0726e01ec19cdbd1a6cd94e4d569eaed328
William Ljungbergh <william.ljungbergh@hotmail.com>
2020-11-13 09:21:00 +0100

    added config script

---
+++ utils/config.py
@@ -0,0 +1,10 @@
+""" 
+    File that contains all the neccessary configuration parameters for the 
+    MPC Trajectory Generation Module
+"""
+
+class Config:
+    def __init__(self):
+        self.vehicle_width = 0.5
+        self.v_max  = 1.5
+        self.omega_max = 1
