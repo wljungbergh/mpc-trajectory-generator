9f966d91ece3dab087b1396277f09585a80575c8
William Ljungbergh <william.ljungbergh@hotmail.com>
2020-12-08 13:50:53 +0100

    moving obstacle

---
+++ src/moving_obstacle.py
@@ -0,0 +1,78 @@
+from path_generator import PathGenerator
+from visibility.graphs import Graphs
+import math
+from matplotlib.gridspec import GridSpec
+from matplotlib.lines import Line2D
+import matplotlib.patches as mpatches
+import matplotlib.pyplot as plt
+from utils.config import Configurator
+from pathlib import Path
+import os
+import numpy as np
+
+graphs = Graphs()
+g = graphs.get_graph(complexity=4)
+file_path = Path(__file__)
+
+config_fn = 'default.yaml'
+yaml_fp = os.path.join(str(file_path.parent.parent), 'configs', config_fn)
+configurator = Configurator(yaml_fp)
+config = configurator.configurate()
+
+path_gen = PathGenerator(config, build=False)
+
+start = list(g.start) + [math.radians(0)]
+end = list(g.end) + [math.radians(0)]
+
+obstacle_radius = 0.5
+obs_pos = [(x, 5.0, obstacle_radius) for x in np.linspace(10,5,int(30/config.ts))]
+
+
+xx,xy,uv,uomega = path_gen.run(g, start, end)
+
+
+
+fig = plt.figure(constrained_layout = True)
+gs = GridSpec(1, 1, figure=fig)
+
+path_ax =  fig.add_subplot(gs[0,0])
+
+
+
+path_ax.plot(start[0], start[1], marker='*', color='g', markersize = 15, label='Start')
+path_ax.plot(end[0], end[1], marker='*', color='r', markersize = 15,label='End')
+path_gen.ppp.plot_all(path_ax)
+path_line, = path_ax.plot([1], '-ob', alpha =0.7, markersize=5)
+path_ax.set_xlabel('X [m]', fontsize = 15)
+path_ax.set_ylabel('Y [m]', fontsize = 15)
+path_ax.axis('equal')
+
+legend_elems = [  Line2D([0], [0], color='k', label='Original Boundary' ),
+                    Line2D([0], [0], color='g', label='Padded Boundary'),
+                    Line2D([0], [0], marker='o', color='b', label='Traversed Path', alpha = 0.5),
+                    Line2D([0], [0], marker='*', color='g', label='Start Position', alpha = 0.5),
+                    Line2D([0], [0], marker='*', color='r', label='End Position'),
+                    mpatches.Patch(color='b', label='Robot'),
+                    mpatches.Patch(color='r', label='Obstacle'),
+                    mpatches.Patch(color='y', label='Padded obstacle')
+                            ]
+path_ax.legend(handles = legend_elems)
+
+for i in range(len(xx)):
+    path_line.set_data(xx[:i], xy[:i])
+    obs = plt.Circle((obs_pos[i][0], obs_pos[i][1]), obstacle_radius, color = 'r', alpha = 1, label='Obstacle')
+    obs_padded = plt.Circle((obs_pos[i][0], obs_pos[i][1]), obstacle_radius + config.vehicle_width/2 + config.vehicle_margin, color = 'y', alpha = 0.7, label='Padded obstacle')
+    veh = plt.Circle((xx[i], xy[i]), config.vehicle_width/2, color = 'b', alpha = 0.7, label='Robot')
+
+    path_ax.add_artist(obs_padded)
+    path_ax.add_artist(obs)
+    path_ax.add_artist(veh)
+
+    
+    
+    plt.draw()
+    plt.pause(config.ts)
+    obs.remove()
+    obs_padded.remove()
+    veh.remove()
+
